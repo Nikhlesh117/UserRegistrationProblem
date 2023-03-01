a2096d702671fac57700224ef0610ad2cd65cd6b
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-01 13:18:09 +0530

    Added: valid First Name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         public static void Main(string[] args)
         {
-            Console.Write("Welcome to the User Registration Problem");
+            Console.Write("Welcome to the User Registration Problem\n");
+
+            Console.WriteLine("1. Enter valid First Name");
+            Console.Write("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Validate validate1 = new Validate();
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter Your First Name : ");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine(validate1.ValidateFirstName(FirstName));
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter correct choice");
+                    break;
+
+            }
         }
     }
 }
---
+++ Validate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Validate
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+    }
+}

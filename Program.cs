8800db65df6f721a5a06c8d280ea379961d392a9
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-03-01 11:20:07 +0530

    Welcome to the User Registration Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Welcome to the User Registration Problem");
+        }
+    }
+}

9cd47681aa7d177df41784bdaa91c809e484ebce
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-01 13:58:39 +0530

    Added: Password Rule 3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,65 @@
-﻿namespace UserRegistration
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistration
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Welcome to the User Registration Problem");
+            Console.Write("Welcome to the User Registration Problem\n");
+
+            Console.WriteLine("1. Enter valid First Name");
+            Console.WriteLine("2. Enter valid Last Name");
+            Console.WriteLine("3. Enter valid email");
+            Console.WriteLine("4. Enter Mobile number along with country code");
+            Console.WriteLine("5. Minimum 8 character password");
+            Console.WriteLine("6. Password previous condition with atleast 1 upper case");
+            Console.WriteLine("7. Password previous conditions with atleast 1 numeric number");
+            Console.Write("Enter your choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Validate validate = new Validate();
+            switch (choice)
+            {
+                case 1:
+                    Console.Write("Enter Your First Name : ");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine(validate.FirstName(FirstName));
+                    break;
+                case 2:
+                    Console.Write("Enter Your Last Name : ");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine(validate.LastName(LastName));
+                    break;
+                case 3:
+                    Console.Write("Enter Your Email : ");
+                    string email = Console.ReadLine();
+                    Console.WriteLine(validate.Email(email));
+                    break;
+                case 4:
+                    Console.Write("Enter Your Phone Number : ");
+                    string PhoneNumber = Console.ReadLine();
+                    Console.WriteLine(validate.Mobile(PhoneNumber));
+                    break;
+                case 5:
+                    Console.WriteLine("Enter Your Password");
+                    string password = Console.ReadLine();
+                    Console.WriteLine(validate.PasswordR1(password));
+                    break;
+                case 6:
+                    Console.WriteLine("Enter Your Password");
+                    string password2 = Console.ReadLine();
+                    Console.WriteLine(validate.PasswordR2(password2));
+                    break;
+                case 7:
+                    Console.WriteLine("Enter Your Password");
+                    string password3 = Console.ReadLine();
+                    Console.WriteLine(validate.PasswordR3(password3));
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct choice");
+                    break;
+
+            }
         }
     }
 }

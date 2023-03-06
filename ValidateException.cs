f3d690a308bf3c3b7ef41050e4f586562288792d
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-06 11:32:25 +0530

    Added: Reflection

---
+++ ValidateException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class ValidateException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_FIRST_NAME,
+            INVALID_LAST_NAME,
+            INVALID_EMAIL,
+            INVALID_MOBILE_NUMBER,
+            INVALID_PASSWORD,
+            NO_SUCH_CLASS, 
+            NO_SUCH_METHOD
+        }
+
+        public ExceptionType type;
+        public ValidateException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
